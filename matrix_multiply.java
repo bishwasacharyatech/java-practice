679517bf12a9350be0e13c8655e343f527b2981f
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-01-04 01:12:39 +0545

    this is matrix multiply

---
+++ matrix_multiply.java
@@ -0,0 +1,38 @@
+//wap to multiply two matrices in java
+public class matrix_multiply {
+    public static void main(String[] args) {
+        int[][] matrixA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] matrixB = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        int[][] result = new int[3][3];
+
+        // Multiplying matrices
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+
+        // Printing the result
+        System.out.println("Resultant Matrix after multiplication:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
