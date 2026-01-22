7bccfbe42bab8e999a6142827bb22bf3def462e6
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-01-22 21:57:30 +0545

    inherentance multi level

--- encapulation_eg.java
+++ encapulation_eg.java
@@ -1,4 +1,5 @@
-class test{
+class test{ 
+    
     private String name;
     private int age;
     public String getName(){
---
+++ inherentance.java
@@ -0,0 +1,25 @@
+// multilevel inheritance 
+class Animal {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+class BabyDog extends Dog {
+    void weep() {
+        System.out.println("weeping...");
+    }
+}
+public class inherentance { 
+
+    public static void main(String[] args) {
+        BabyDog babyDog = new BabyDog();
+        babyDog.weep();
+        babyDog.bark();
+        babyDog.eat();
+    }
+}
