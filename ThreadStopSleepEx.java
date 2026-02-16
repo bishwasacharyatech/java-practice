621d806aa834bcc293b513ae36669684ca8bd288
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-02-16 07:00:28 +0545

    thread stop and sleep method

---
+++ ThreadStopSleepEx.java
@@ -0,0 +1,42 @@
+class ThreadA extends Thread{
+    public void run(){
+       for(int i=1;i<=5;i++){
+        System.out.println("Running Thread "+i+" from class A");
+          // if(i==3) stop();
+        if(i==3) interrupt();//change in stop() method 
+       }
+       System.out.println("exit from class A");
+
+    }
+}
+class ThreadB extends Thread{
+    public void run(){
+       for(int j=1;j<=5;j++){
+        System.out.println("Running Thread "+j+" from class B");
+        if (j==2)
+            try {
+                sleep(1000);//in miniseconds
+            } catch (Exception e) {
+                System.out.println(e);
+
+            }
+        }
+         System.out.println("exit from class B");
+    }
+}
+class ThreadC extends Thread{
+    public void run(){
+       for(int k=1;k<=5;k++){
+        System.out.println("Running Thread "+k+" from class C");
+        }
+         System.out.println("exit from class C");
+    }
+}
+public class ThreadStopSleepEx {
+    public static void main(String[] args) {
+        new ThreadA().start();
+        new ThreadB().start();  
+        new ThreadC().start();
+
+    }
+}
