5812cd24f8fd348f3d30b0a78272ced7f330a862
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-02-16 07:08:10 +0545

    thread with ThreadPriority

---
+++ ThreadPriority.java
@@ -0,0 +1,50 @@
+class ThreadA extends Thread{
+    public void run(){
+       for(int i=1;i<=5;i++){
+        System.out.println("Running Thread "+i+" from class A");
+          // if(i==3) stop();
+        if(i==3) interrupt();//change in stop() method 
+       }
+       System.out.println("exit from class A");
+
+    }
+}
+class ThreadB extends Thread{
+    public void run(){
+       for(int j=1;j<=5;j++){
+        System.out.println("Running Thread "+j+" from class B");
+        if (j==2)
+            try {
+                sleep(1000);//in miniseconds
+            } catch (Exception e) {
+                System.out.println(e);
+
+            }
+        }
+         System.out.println("exit from class B");
+    }
+}
+class ThreadC extends Thread{
+    public void run(){
+       for(int k=1;k<=5;k++){
+        System.out.println("Running Thread "+k+" from class C");
+        }
+         System.out.println("exit from class C");
+    }
+}
+public class ThreadPriority {
+    public static void main(String[] args) {
+        ThreadA t1 = new ThreadA();
+        ThreadB t2 = new ThreadB();
+        ThreadC t3 = new ThreadC();
+        t1.setPriority(10);
+        t2.setPriority(5);
+        t3.setPriority(1);
+        t1.start();
+        t2.start();  
+        t3.start();
+
+    }
+
+    
+}
