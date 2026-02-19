a861e7050bbe7e2a1ae762cfc2ed42e99a8827d3
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-02-19 21:52:45 +0545

    string char at

---
+++ CharAtExample.java
@@ -0,0 +1,18 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        String word = "Gemini";
+
+        char first = word.charAt(0);
+        
+     
+        char last = word.charAt(word.length() - 1);
+
+        System.out.println("First letter: " + first); 
+        System.out.println("Last letter: " + last);  
+
+        System.out.println("--- Printing all characters ---");
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println("Index " + i + " is: " + word.charAt(i));
+        }
+    }
+}
