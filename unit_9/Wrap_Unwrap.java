2fb403a17bacd2f66a0cde65348eadc65365979f
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-02-26 06:31:57 +0545

    unwrap in java

--- unit_9/Wrap_Unwrap.java
+++ unit_9/Wrap_Unwrap.java
@@ -3,8 +3,16 @@ package unit_9;
 public class Wrap_Unwrap {
     public static void main(String[] args) {
         int a=10;
-        Integer obji = Integer.valueof(a);
-        System.out.rojnt
+        Integer obji = Integer.valueOf(a);
+        System.out.println("The value of obji is: " + obji);
+        Integer obji2 = a; // Auto-boxing
+        System.out.println("The value of obji2 is: " + obji2);
+        int b = obji; // Auto-unboxing
+        System.out.println("The value of b is: " + b);
+        int c = obji.intValue(); // Explicit unboxing
+        System.out.println("The value of c is: " + c);
+        
+
 
 
 
