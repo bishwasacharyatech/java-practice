ffac3cf5a4657f018783e220b133c498bd989e45
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-03-02 21:22:18 +0545

    stream character

---
+++ unit_8/character_streamEx.java
@@ -0,0 +1,26 @@
+package unit_8;
+
+import java.io.*;
+
+public class character_streamEx {
+    public static void main(String[] args) {
+       try {
+            FileWriter writer = new FileWriter("bishwas.txt");
+            BufferedWriter bw =new BufferedWriter(writer);
+            bw.write("Hello, this is a test file.");
+            bw.write("This file is created using BufferedWriter.");
+             
+            System.out.println("File writing completed successfully.");
+            bw.close();
+            writer.close();
+                FileReader reader = new FileReader("bishwas.txt");
+            BufferedReader br =new BufferedReader(reader);
+            String data=br.readLine();
+            System.out.println("Data read from file: " + data);
+            br.close();
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e);
+
+    }
+}}
