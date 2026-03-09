bb61831a6a460f991ec609169932ea4d847f4de5
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-03-09 07:02:55 +0545

    bytes stream

---
+++ unit_8/Bytes_stream.java
@@ -0,0 +1,22 @@
+package unit_8;
+
+import java.io.*;
+public class Bytes_stream {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream fos = new FileOutputStream("bishwas.txt");
+           int content= 1011011;
+           fos.write((byte) content);
+            System.out.println("File writing completed successfully.");
+            fos.close();
+
+            FileInputStream fis = new FileInputStream("bishwas.txt");
+            int data = fis.read();
+            System.out.println("Data read from file: " + data);
+            fis.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    
+}
+}
