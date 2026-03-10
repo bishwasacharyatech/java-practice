aa6666b3aec4a48c23719bffa168a0b8ee49ce3a
bishwasacharyatech <bishwasacharya.tech@gmail.com>
2026-03-10 15:35:58 +0545

    array example

--- unit_10/Array_ex.java
+++ unit_10/Array_ex.java
@@ -2,29 +2,31 @@ package unit_10;
 import java.util.Scanner;
 public class Array_ex {
     public static void main(String[] args) {
-        int a[] = new int[5];
+        int a[] = new int[8];
         int i,max,min;
         Scanner Scan = new Scanner(System.in);
-        System.out.println("Enter 5 number");
-        for(i=0; i<5; i++){
+        System.out.println("Enter  6 number");
+        for(i=0; i<6; i++){
              a[i]=Scan.nextInt();
-             Scan.close();
+             
 
 
     }
+    
     max=a[0];
-    min=a[0];
-    for(i=1; i<5; i++){
+    min=a[0]; 
+    for(i=1; i<6; i++){
             if(a[i]>max)
                 max=a[i];
         }
-         for(i=1; i<5; i++){
+         for(i=1; i<6; i++){
             if(a[i]<min)
                 min=a[i];
 }
     System.out.println("main value is : "+max);
 
     System.out.println("main value is : "+min);
+    Scan.close();
     
 }
 }
